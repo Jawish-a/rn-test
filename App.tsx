2635734f1a2d127884e1781387fe29857650f9af
Ali Jawish <alijawish@Alis-MacBook-Pro.local>
2022-10-18 16:30:52 +0300

    firease integration

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import 'react-native-reanimated';
-import React, {Suspense} from 'react';
+import React, {Suspense, useRef} from 'react';
 // import useAppState from 'react-native-appstate-hook';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Main from './src/screens/Main';
@@ -8,6 +8,7 @@ import {NavigationContainer} from '@react-navigation/native';
 // import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {LoadingScreen} from './src/screens/Loading.screen';
 // import {LogBox} from 'react-native';
+import analytics from '@react-native-firebase/analytics';
 
 // LogBox.ignoreLogs([
 //   "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
@@ -17,13 +18,32 @@ import {LoadingScreen} from './src/screens/Loading.screen';
 
 const App = () => {
   const queryClient = new QueryClient();
+  // for Firebase screen tracking
+  const routeNameRef = useRef<any>(null);
+  const navigationRef = useRef<any>(null);
 
   // useAppState({
   //   onChange: onAppStateChange,
   // });
 
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current!.getCurrentRoute()?.name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current.getCurrentRoute().name;
+
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+        routeNameRef.current = currentRouteName;
+      }}>
       <QueryClientProvider client={queryClient}>
         <Suspense fallback={<LoadingScreen />}>
           <Main />
---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Ali Jawish on 17/10/2022.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
